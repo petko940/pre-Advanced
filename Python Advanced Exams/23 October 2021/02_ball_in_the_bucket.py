caf758c2875fa8d7b5fe545ad1559f5a14b6ea57
petko <p.todorov1015@gmail.com>
2022-12-29 12:50:51 +0200

    done

---
+++ Python Advanced Exams/23 October 2021/02_ball_in_the_bucket.py
@@ -0,0 +1,28 @@
+matrix = [[x for x in input().split()] for x in range(6)]
+
+points = 0
+for _ in range(3):
+    throw = input().split(", ")
+    row = int(throw[0][1:])
+    col = int(throw[1][:-1])
+    try:
+        symbol = matrix[row][col]
+    except IndexError:
+        continue
+
+    if matrix[row][col] == "B":
+        matrix[row][col] = "0"
+        for i in range(6):
+            points += int(matrix[i][col])
+
+if points < 100:
+    print(f"Sorry! You need {100 - points} points more to win a prize.")
+else:
+    reward = None
+    if points < 200:
+        reward = "Football"
+    elif points < 300:
+        reward = "Teddy Bear"
+    else:
+        reward = "Lego Construction Set"
+    print(f"Good job! You scored {points} points, and you've won {reward}.")
